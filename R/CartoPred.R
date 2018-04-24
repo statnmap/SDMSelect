817dfd12103fc928c13c346438ac8154a1c3ce8a
Sébastien <sebastien@thinkr.fr>
2018-04-24 16:23:15 +0200

    gplot_data for single layer raster

--- R/CartoPred.R
+++ R/CartoPred.R
@@ -454,35 +454,3 @@ Map_predict <- function(object, saveWD, mask = NULL, Num = NULL,
   }
   return(pred_map)
 } # end of function
-
-#' Transform raster as data.frame to be later used with ggplot
-#' Modified from rasterVis::gplot
-#'
-#' @param x A Raster* object
-#' @param maxpixels Maximum number of pixels to use
-#'
-#' @details rasterVis::gplot is nice to plot a raster in a ggplot but
-#' if you want to plot different rasters on the same plot, you are stuck.
-#' If you want to add other information or transform your raster as a
-#' category raster, you can not do it. With `SDMSelect::gplot_data`, you retrieve your
-#' raster as a data.frame that can be modified as wanted using `dplyr` and
-#' then plot in `ggplot` using `geom_tile`.
-#' If Raster has levels, they will be joined to the final tibble.
-#'
-#' @export
-
-gplot_data <- function(x, maxpixels = 50000)  {
-  x <- raster::sampleRegular(x, maxpixels, asRaster = TRUE)
-  coords <- raster::xyFromCell(x, seq_len(raster::ncell(x)))
-  ## Extract values
-  dat <- utils::stack(as.data.frame(raster::getValues(x)))
-  names(dat) <- c('value', 'variable')
-
-  dat <- dplyr::as.tbl(data.frame(coords, dat))
-
-  if (!is.null(levels(x))) {
-    dat <- dplyr::left_join(dat, levels(x)[[1]],
-                            by = c("value" = "ID"))
-  }
-  dat
-}
